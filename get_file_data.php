706a77e0112bec37bec5de9452a4d7bd7d617c4a
DomantasAug <94737304+DomantasAug@users.noreply.github.com>
2021-11-21 12:12:13 +0200

    Adding json, csv functionality

--- get_file_data.php
+++ get_file_data.php
@@ -18,18 +18,16 @@ class xml_loader implements loaderInterface {
     public function load() {
         //get file contents and save
         $this->xml = new SimpleXMLElement($this->file);
-
-         
+  
     }
     
     public function show() {
         echo "
-        <table>
-            <thead>
+        <table><thead>
             <th>First name</th>
             <th>Age</th>
             <th>Gender</th>
-            </thead>";
+        </thead>";
         
         echo "<tbody>"; //
         //a loop to render items from xml file to webpage
@@ -39,17 +37,16 @@ class xml_loader implements loaderInterface {
             echo "<td>".$item->age."</td>";
             echo "<td>".$item->gender."</td>";
             echo "</tr>";
-            
         }
                 
-        echo "</table>";             
-    }
-    
+        echo "</tbody></table>";           
+    }   
 }
 
 
 class json_loader implements loaderInterface {
     private $file;
+    private $data;
     
     public function __construct($file) {
         $this->file = $file;
@@ -57,28 +54,65 @@ class json_loader implements loaderInterface {
     
     public function load() {
         //get file contents and save
+        $this->data = json_decode($this->file);
           
     }
     
     public function show() {
+        echo "
+        <table><thead>
+            <th>First name</th>
+            <th>Age</th>
+            <th>Gender</th>
+        </thead>";
         
+        echo "<tbody>"; 
+        foreach ($this->data as $item)
+        {
+            echo "<tr>";
+            echo "<td>".$item->first_name."</td>";
+            echo "<td>".$item->age."</td>";
+            echo "<td>".$item->gender."</td>";
+            echo "</tr>";
+            
+        }
+        echo "</tbody></table>";
     }
 }
 
 class csv_loader implements loaderInterface {
     private $file;
+    private $data;
     
     public function __construct($file) {
         $this->file = $file;
     }
     
     public function load() {
-        //get file contents and save
-        
+        //reading csv file as a string and spliting the string into array by new lines
+        $this->data = explode ("\r\n", $this->file);
+        //removing first array line - collumn names and leaving only the data
+        array_shift($this->data);
     }
     
     public function show() {
+        echo "
+        <table><thead>
+            <th>First name</th>
+            <th>Age</th>
+            <th>Gender</th>
+        </thead>";
         
+        echo "<tbody>";
+        foreach($this->data as $line){ // Loop over each line
+            $column = explode(",", $line); // split the line in 'columns'
+            echo "<tr>";
+            echo "<td>".$column[0]."</td>";
+            echo "<td>".$column[1]."</td>";
+            echo "<td>".$column[2]."</td>";
+            echo "</tr>";
+        }
+        echo "</tbody></table>";
     }
 }
 ?>
--- showfile.php
+++ showfile.php
@@ -1,5 +1,8 @@
 <?php
 
+include "index.html";
+include "get_file_data.php";
+
 //getting the file from a post request
 $file = file_get_contents($_FILES['fileToUpload']['tmp_name']);
 $fileName = $_FILES['fileToUpload']['name'];
@@ -8,10 +11,9 @@ $ext = pathinfo($fileName, PATHINFO_EXTENSION);
 //setting up which class to call, depending on file type
 $loader_type = $ext."_loader";
 
-include "get_file_data.php";
 
 try {
-    $loader = new $loader_type(file_get_contents($_FILES['fileToUpload']['tmp_name']));
+    $loader = new $loader_type($file);
     $loader->load();
     $loader->show();
     
